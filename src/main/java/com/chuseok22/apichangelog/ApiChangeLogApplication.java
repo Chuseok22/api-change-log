e265c9d277243b9a515b097eab03333a00e6eca5
Chuseok22 <bjh59629@naver.com>
2025-06-16 13:20:09 +0900

    project init

---
+++ src/main/java/com/chuseok22/apichangelog/ApiChangeLogApplication.java
@@ -0,0 +1,13 @@
+package com.chuseok22.apichangelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiChangeLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiChangeLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chuseok22/apichangelog/ApiChangeLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuseok22.apichangelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiChangeLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
